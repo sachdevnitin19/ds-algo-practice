a7e46a12a3ca2e039142bdb95d2938a3872c45cb
sachdevnitin <nitin@forkmedia.in>
2019-05-29 14:23:46 +0530

    changes

--- Queue.java
+++ Queue.java
@@ -3,7 +3,7 @@ import java.io.*;
 class Queue {
     static final int MAX = 10;
     int front, back;
-    char qArr[] = new char[MAX];
+    int qArr[] = new int[MAX];
 
     Queue() {
         front = -1;
@@ -11,36 +11,62 @@ class Queue {
     }
 
     void enqueue(int qElem) {
-        if ((front == MAX - 1 && back == 0) || (back > front && front == back - 1)) {
+        if ((back == MAX - 1 && front == 0) || (front > back && back == front - 1)) {
             System.out.println("Queue full");
         } else {
             if (front == -1 && back == -1) {
                 front++;
                 back++;
-            }else if(front<MAX-1){
-                front++;
-            }else if(back>0 && front!=back-1){
-                if(front==Max-1) front=0;
-                else front++;
+            } else if (back < MAX - 1) {
+                back++;
+            } else if (front > 0 && back != front - 1) {
+                if (back == MAX - 1)
+                    back = 0;
+                else
+                    back++;
             }
-            qArr[front] = qElem;
+            qArr[back] = qElem;
         }
     }
-    void printQueue(){
-        int frontCopy=front,backCopy=back;
-        if(front==-1 && back==-1) System.out.println("Queue is Empty");
-        else{
-            if(front>back){
-                while(frontCopy>=backCopy){
-                    System.out.print(qArr[frontCopy--]+"\t");
+
+    int dequeue() {
+        int tempInt = -1;
+        if (front != -1 && back != -1) {
+            if (front == back) {
+                tempInt = qArr[front];
+                front = -1;
+                back = -1;
+            } else if (front >= 0 && front < back) {
+                tempInt = qArr[front++];
+            } else if (front > 0 && front > back) {
+                if (front == MAX - 1) {
+                    tempInt = qArr[front];
+                    front = 0;
                 }
-            }else if(back>front){
-                while(frontCopy>=0){
-                    System.out.print(qArr[frontCopy--]+"\t");
+                tempInt = qArr[front++];
+            }
+        } else {
+            System.out.println("Queue is empty");
+        }
+        return tempInt;
+    }
+
+    void printQueue() {
+        int frontCopy = front, backCopy = back;
+        if (front == -1 && back == -1)
+            System.out.println("Queue is Empty");
+        else {
+            if (front <= back) {
+                while (frontCopy <= backCopy) {
+                    System.out.print(qArr[frontCopy++] + "\t");
                 }
-                backCopy=MAX-1;
-                while(backCopy>=back){
-                    System.out.print(qArr[backCopy--]+"\t");
+            } else if (front > back) {
+                while (frontCopy <= MAX - 1) {
+                    System.out.print(qArr[frontCopy++] + "\t");
+                }
+                backCopy = 0;
+                while (backCopy <= back) {
+                    System.out.print(qArr[backCopy++] + "\t");
                 }
             }
         }
@@ -48,10 +74,38 @@ class Queue {
 
 }
 
-class Main {
+class QDriver {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue myQ = new Queue();
+        System.out.println("1. enqueue");
+        System.out.println("2. dequeue");
+        System.out.println("3. print queue");
+        System.out.println("0. Exit");
+        int choice = Integer.parseInt(br.readLine());
+        while (choice != 0) {
 
+            switch (choice) {
+            case 1:
+                myQ.enqueue(Integer.parseInt(br.readLine()));
+                break;
+            case 2:
+                int newElem = myQ.dequeue();
+                if (newElem != -1)
+                    System.out.println("Elem from Q:-" + newElem);
+                break;
+            case 3:
+                myQ.printQueue();
+                break;
+            }
+            System.out.println("");
+            System.out.println("1. enqueue");
+            System.out.println("2. dequeue");
+            System.out.println("3. print queue");
+            System.out.println("0. Exit");
+            System.out.println("");
+            choice = Integer.parseInt(br.readLine());
+        }
     }
 }
