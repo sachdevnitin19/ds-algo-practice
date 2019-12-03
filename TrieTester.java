9cdb7d2f601e7548a2cbcb58b17244ff0871bab1
sachdevnitin <nitin@forkmedia.in>
2019-12-03 19:32:52 +0530

    added trie soln

---
+++ TrieTester.java
@@ -0,0 +1,25 @@
+import com.nitin.utils.Trie;
+
+class TrieTester{
+    public static void main(String args[]){
+        Trie dict=new Trie();
+        // dict.insert("app");
+        dict.insert("apple");
+        // dict.insert("beer");
+
+        // dict.insert("add");
+        // dict.insert("jam");
+        // dict.insert("rental");
+
+        // dict.insert("apostle");
+        // dict.insert("apostrophe");
+
+        System.out.println(dict.search("apple"));
+        // System.out.println(dict.startsWith("ma"));
+        // System.out.println(dict.startsWith("so"));
+        // System.out.println(dict.search("nitin"));
+        // System.out.println(dict.search("manuel"));
+
+        // System.out.println(dict.search("university"));
+    }
+}
