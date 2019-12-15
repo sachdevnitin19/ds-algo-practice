5f3b5ad4d95f0e98824d72253c562058d9af5c10
jaisam <jaisamtani123@gmail.com>
2019-12-15 18:02:45 +0530

    implemented detect cycle in directed graph

--- AdjacencyListGraphTester.java
+++ AdjacencyListGraphTester.java
@@ -2,18 +2,45 @@ import com.nitin.utils.AdjacencyListGraph;
 
 class AdjacencyListGraphTester {
     public static void main(String args[]) {
-        AdjacencyListGraph MyGraph=new AdjacencyListGraph(5);
+        AdjacencyListGraph MyGraph=new AdjacencyListGraph(6,false);
 
         MyGraph.addEdge(0, 1);
         MyGraph.addEdge(0, 4);
-        MyGraph.addEdge(1, 4);
-        MyGraph.addEdge(1, 3);
+        // MyGraph.addEdge(1, 4);
+        // MyGraph.addEdge(1, 3);
         MyGraph.addEdge(1, 2);
         MyGraph.addEdge(4, 3);
-        MyGraph.addEdge(3, 2);
+        // MyGraph.addEdge(3, 2);
 
         MyGraph.printBFS();
 
         MyGraph.printDFS();
+        MyGraph.printDFSRecursively();
+        
+        AdjacencyListGraph MyGraph2=new AdjacencyListGraph(8, true);
+
+        // MyGraph2.addEdge(5, 2);
+        // MyGraph2.addEdge(5, 1);
+        // // MyGraph2.addEdge(5, 0);
+        // MyGraph2.addEdge(0, 5);
+        // // MyGraph2.addEdge(2, 0);
+        // MyGraph2.addEdge(1, 0);
+        // MyGraph2.addEdge(1, 4);
+        // MyGraph2.addEdge(1, 7);
+        // MyGraph2.addEdge(0, 4);
+        // MyGraph2.addEdge(3, 6);
+
+        MyGraph2.addEdge(0, 1); 
+        MyGraph2.addEdge(0, 2); 
+        MyGraph2.addEdge(1, 2); 
+        MyGraph2.addEdge(2, 0); 
+        MyGraph2.addEdge(2, 3); 
+        MyGraph2.addEdge(3, 3); 
+
+        // String topOrder=MyGraph2.returnTopologicalOrder();
+        // System.out.println("Topological Order= "+topOrder);
+        System.out.println("DFS:- ");
+        MyGraph2.printDFSRecursively();
+        System.out.println(MyGraph2.isGraphCyclic()?"Graph is cyclic.":"Graph is acyclic.");
     }
 }
--- com/nitin/utils/AdjacencyListGraph.java
+++ com/nitin/utils/AdjacencyListGraph.java
@@ -9,9 +9,11 @@ import java.util.Stack;
 public class AdjacencyListGraph {
     int noOfVertices;
     SinglyLinkedList[] adjList;
+    boolean isGraphDirected;
 
-    public AdjacencyListGraph(int noOfVertices) {
+    public AdjacencyListGraph(int noOfVertices, boolean isDirected) {
         this.noOfVertices = noOfVertices;
+        this.isGraphDirected = isDirected;
         this.adjList = new SinglyLinkedList[noOfVertices];
         for (int i = 0; i < noOfVertices; i++) {
             this.adjList[i] = new SinglyLinkedList();
@@ -20,7 +22,10 @@ public class AdjacencyListGraph {
 
     public void addEdge(int src, int dest) {
         this.adjList[src].insert(dest);
-        this.adjList[dest].insert(src);
+        if (!isGraphDirected) {
+            this.adjList[dest].insert(src);
+        }
+
     }
 
     public void printBFS() {
@@ -56,17 +61,121 @@ public class AdjacencyListGraph {
                 while (!nodesStack.isEmpty()) {
                     int currNode = nodesStack.pop();
                     if (!nodesVisited.contains(currNode)) {
-                        System.out.print(currNode+" ");
+                        System.out.print(currNode + " ");
                         nodesVisited.add(currNode);
-                        Node ptr=this.adjList[currNode].head;
-                        while(ptr!=null){
+                        Node ptr = this.adjList[currNode].head;
+                        while (ptr != null) {
                             nodesStack.push(ptr.data);
-                            ptr=ptr.next;
+                            ptr = ptr.next;
                         }
                     }
                 }
             }
         }
         System.out.println();
+    }
+
+    public void printDFSRecursively() {
+        HashSet<Integer> nodesVisited = new HashSet<Integer>();
+        for (int i = 0; i < noOfVertices; i++) {
+            dfsTraverse(i, nodesVisited);
+        }
+        System.out.println();
+    }
+
+    public void dfsTraverse(int vertice, HashSet<Integer> nodesVisited) {
+        if (!nodesVisited.contains(vertice)) {
+            System.out.print(vertice + " ");
+            nodesVisited.add(vertice);
+            Node ptr = this.adjList[vertice].head;
+            while (ptr != null) {
+                dfsTraverse(ptr.data, nodesVisited);
+                ptr = ptr.next;
+            }
+
+        }
+    }
+
+    public boolean isGraphCyclic() {
+        HashSet<Integer> nodesVisited = new HashSet<Integer>();
+        HashSet<Integer> recursionSet = new HashSet<Integer>();
+        for (int i = 0; i < noOfVertices; i++) {
+            if (isGraphCyclicUtil(i, nodesVisited, recursionSet)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isGraphCyclicUtil(int vertice, HashSet<Integer> nodesVisited, HashSet<Integer> recursionSet) {
+        if (recursionSet.contains(vertice)) {
+            return true;
+        }
+        
+        recursionSet.add(vertice);
+        Node ptr = this.adjList[vertice].head;
+        while (ptr != null) {
+            if (!nodesVisited.contains(ptr.data)) {
+                if (isGraphCyclicUtil(ptr.data, nodesVisited, recursionSet)) {
+                    return true;
+                }
+            }
+
+            ptr = ptr.next;
+        }
+        nodesVisited.add(vertice);
+        recursionSet.remove(vertice);
+        return false;
+    }
+
+    public String returnTopologicalOrder() {
+        if (!isGraphDirected) {
+            System.out.println("Topological sort order cannot be found for undirected graph");
+            return "";
+        }
+        HashSet<Integer> nodesVisited = new HashSet<Integer>();
+        Stack<Integer> topologicalOrderStack = new Stack<Integer>();
+        try {
+            for (int i = 0; i < noOfVertices; i++) {
+                if (!nodesVisited.contains(i)) {
+                    findTopologicalOrder(i, nodesVisited, topologicalOrderStack);
+                }
+            }
+        } catch (CyclicGraphException e) {
+            System.out.println(e.message);
+            return "";
+        }
+
+        StringBuilder order = new StringBuilder();
+        while (!topologicalOrderStack.isEmpty()) {
+            order.append(topologicalOrderStack.pop() + " ");
+        }
+        return order.toString().trim();
+    }
+
+    public void findTopologicalOrder(int vertice, HashSet<Integer> nodesVisited, Stack<Integer> topologicalOrderStack)
+            throws CyclicGraphException {
+        if (nodesVisited.contains(vertice)) {
+            throw new CyclicGraphException("Graph contains cycle");
+        }
+        nodesVisited.add(vertice);
+        Node ptr = this.adjList[vertice].head;
+        while (ptr != null) {
+            if (!nodesVisited.contains(ptr.data)) {
+                findTopologicalOrder(ptr.data, nodesVisited, topologicalOrderStack);
+            }
+            ptr = ptr.next;
+        }
+        topologicalOrderStack.push(vertice);
+    }
+
+    class CyclicGraphException extends Exception {
+        private static final long serialVersionUID = 7718828512143293558L;
+        public String message;
+
+        CyclicGraphException(String message) {
+            super(message);
+            this.message = message;
+        }
     }
 }
