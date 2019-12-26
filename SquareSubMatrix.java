3c1f033dff6e8ee6f8a2c2223e96e03d252d4d74
sachdevnitin <nitin@forkmedia.in>
2019-12-26 19:32:47 +0530

    improved comments

--- SquareSubMatrix.java
+++ SquareSubMatrix.java
@@ -1,11 +1,7 @@
 class SquareSubMatrix {
     public static void main(String args[]) {
-        boolean testArr[][]={
-            {true,true,false},
-            {true,true,false},
-            {true,true,false}
-        };
-        System.out.println("Max length:-"+findLargestSquare(testArr));
+        boolean testArr[][] = { { true, true, false }, { true, true, false }, { true, true, false } };
+        System.out.println("Max length:-" + findLargestSquare(testArr));
     }
 
     public static int findLargestSquare(boolean[][] sqArr) {
@@ -17,26 +13,29 @@ class SquareSubMatrix {
             return 0;
 
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < noOfRows; i++) {
-            for (int j = 0; j < noOfColumns; j++) {
-                //computing for each cell
-                if (sqArr[i][j])
-                    max = Math.max(max, findLargestSquare(sqArr, i, j));
+        for (int rowNo = 0; rowNo < noOfRows; rowNo++) {
+            for (int colNo = 0; colNo < noOfColumns; colNo++) {
+                //iterating through matrix and if current cell is true,
+                //it can be part of larger submatrix with true values
+                if (sqArr[rowNo][colNo])
+                    max = Math.max(max, findLargestSquare(sqArr, rowNo, colNo));
             }
         }
         return max;
     }
 
-    public static int findLargestSquare(boolean[][] sqArr, int i, int j) {
-        //if current cell is in last row or column then it cannot be upper left cell of square submatrix.
-        if (i == sqArr.length || j == sqArr[0].length)
+    public static int findLargestSquare(boolean[][] sqArr, int rowNo, int colNo) {
+        //if recursive call is for out of boundary of row or column, iniated by any cell in last column or last row
+        if (rowNo == sqArr.length || colNo == sqArr[0].length)
             return 0;
-        
+
         //if current cell is false then it cannot be upper left cell of a square submatrix.
-        if (!sqArr[i][j])
+        if (!sqArr[rowNo][colNo])
             return 0;
-        
+
         // recursively finding whether right, bottom and right-bottom are part of square submatrix.
-        return 1+Math.min(Math.min(findLargestSquare(sqArr, i+1, j),findLargestSquare(sqArr, i, j+1)),findLargestSquare(sqArr, i+1, j+1));
+        return 1 + Math.min(
+                Math.min(findLargestSquare(sqArr, rowNo + 1, colNo), findLargestSquare(sqArr, rowNo, colNo + 1)),
+                findLargestSquare(sqArr, rowNo + 1, colNo + 1));
     }
 }
