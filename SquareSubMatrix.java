243341c8019823f5185b3b0520168f5a5158c1f0
sachdevnitin <nitin@forkmedia.in>
2019-12-27 13:21:36 +0530

    optimized SquareSubMatrix solution

--- SquareSubMatrix.java
+++ SquareSubMatrix.java
@@ -1,6 +1,10 @@
 class SquareSubMatrix {
     public static void main(String args[]) {
-        boolean testArr[][] = { { true, true, false }, { true, true, false }, { true, true, false } };
+        boolean testArr[][] = { 
+            { true, false, false },
+            { true, false, false },
+            { true, false, false } 
+        };
         System.out.println("Max length:-" + findLargestSquare(testArr));
     }
 
@@ -11,6 +15,7 @@ class SquareSubMatrix {
         int noOfColumns = sqArr[0].length;
         if (noOfColumns == 0)
             return 0;
+        int[][] cache = new int[noOfRows][noOfColumns];
 
         int max = Integer.MIN_VALUE;
         for (int rowNo = 0; rowNo < noOfRows; rowNo++) {
@@ -18,13 +23,13 @@ class SquareSubMatrix {
                 //iterating through matrix and if current cell is true,
                 //it can be part of larger submatrix with true values
                 if (sqArr[rowNo][colNo])
-                    max = Math.max(max, findLargestSquare(sqArr, rowNo, colNo));
+                    max = Math.max(max, findLargestSquare(sqArr, cache, rowNo, colNo));
             }
         }
         return max;
     }
 
-    public static int findLargestSquare(boolean[][] sqArr, int rowNo, int colNo) {
+    public static int findLargestSquare(boolean[][] sqArr, int[][] cache, int rowNo, int colNo) {
         //if recursive call is for out of boundary of row or column, iniated by any cell in last column or last row
         if (rowNo == sqArr.length || colNo == sqArr[0].length)
             return 0;
@@ -33,9 +38,13 @@ class SquareSubMatrix {
         if (!sqArr[rowNo][colNo])
             return 0;
 
+        if(cache[rowNo][colNo]>0)
+            return cache[rowNo][colNo];
         // recursively finding whether right, bottom and right-bottom are part of square submatrix.
-        return 1 + Math.min(
-                Math.min(findLargestSquare(sqArr, rowNo + 1, colNo), findLargestSquare(sqArr, rowNo, colNo + 1)),
-                findLargestSquare(sqArr, rowNo + 1, colNo + 1));
+        cache[rowNo][colNo]=1 + Math.min(
+                Math.min(findLargestSquare(sqArr, cache, rowNo + 1, colNo),
+                        findLargestSquare(sqArr, cache, rowNo, colNo + 1)),
+                findLargestSquare(sqArr, cache, rowNo + 1, colNo + 1));
+        return cache[rowNo][colNo];
     }
 }
