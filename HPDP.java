ae72e72b33a8ef641bda65ab58240fff4e9febbe
Nitin Sachdev <nitin@forkmediagroup.com>
2020-04-28 21:29:48 +0530

    minor changes

--- HPDP.java
+++ HPDP.java
@@ -1,93 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.io.*;
 
-public class HPDP {
-
+class HPDP {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int tt = 1; tt <= t; tt++) {
+        int testCases = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+            int noOfBuildings = Integer.parseInt(st.nextToken());
+            int noOfPaints = Integer.parseInt(st.nextToken());
+            int splOfLoc = Integer.parseInt(st.nextToken());
 
-            int a[] = new int[n];
+            int buildingPaint[] = new int[noOfBuildings];
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(st.nextToken());
+            for (int building = 0; building < noOfBuildings; building++) {
+                buildingPaint[building] = Integer.parseInt(st.nextToken());
             }
 
-            int cost[][] = new int[n][m];
-            for (int j = 0; j < n; j++) {
+            int buildingPaintCost[][] = new int[noOfBuildings][noOfPaints];
+            for (int row = 0; row < noOfBuildings; row++) {
                 st = new StringTokenizer(br.readLine());
-                for (int kk = 0; kk < m; kk++) {
-                    cost[j][k] = Integer.parseInt(st.nextToken());
+                for (int col = 0; col < noOfPaints; col++) {
+                    buildingPaintCost[row][col] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            long dp[][][] = compute(a, cost, n, k, m);
-
-            long ans = Long.MAX_VALUE;
-
-            for (int i = 0; i < m; i++) {
-
-                if (dp[n - 1][k - 1][i] != -1) {
-                    ans = Math.min(ans, dp[n - 1][k - 1][i]);
-                }
-            }
-
-            if (ans == Long.MAX_VALUE)
-                System.out.println("-1");
-            else
-                System.out.println(ans);
-
+            System.out.println(minCostToPaint(buildingPaint, buildingPaintCost, noOfBuildings, noOfPaints, splOfLoc));
         }
     }
 
-    private static long[][][] compute(int a[], int cost[][], int n, int k, int m) {
+    private static long minCostToPaint(int buildingPaint[], int buildingPaintCost[][], int noOfBuildings,
+            int noOfPaints, int splOfLoc) {
 
-        long dp[][][] = new long[n][k][m];
+        long memoization[][][] = new long[noOfBuildings][splOfLoc][noOfPaints];
 
-        for (int apar = 0; apar < n; apar++) {
-            boolean curr_apar_painted = a[apar] != 0;
+        for (int apar = 0; apar < noOfBuildings; apar++) {
+            boolean isCurrAptPainted = buildingPaint[apar] != 0;
 
-            for (int score = 0; score < k; score++) {
+            for (int score = 0; score < splOfLoc; score++) {
 
-                for (int paint = 0; paint < m; paint++) {
+                for (int paint = 0; paint < noOfPaints; paint++) {
 
                     if (score > apar) {
-                        dp[apar][score][paint] = -1L;
+                        memoization[apar][score][paint] = -1L;
                         continue;
                     }
-                    if (curr_apar_painted && (paint != a[apar] - 1)) {
-                        dp[apar][score][paint] = -1L;
+                    if (isCurrAptPainted && (paint != buildingPaint[apar] - 1)) {
+                        memoization[apar][score][paint] = -1L;
                         continue;
                     }
                     if (apar == 0) {
-                        dp[apar][score][paint] = cost[apar][a[apar] = 1];
+                        if (!isCurrAptPainted) {
+                            memoization[apar][score][paint] = buildingPaintCost[apar][paint];
+                        }
+
                     } else {
                         long val = Long.MAX_VALUE;
-                        for (int prev_paint = 0; prev_paint < m; prev_paint++) {
+                        for (int prev_paint = 0; prev_paint < noOfPaints; prev_paint++) {
                             if (prev_paint == paint) {
-                                if (dp[apar - 1][score][prev_paint] != -1)
-                                    val = Math.min(dp[apar - 1][score][prev_paint], val);
+                                if (memoization[apar - 1][score][prev_paint] != -1)
+                                    val = Math.min(memoization[apar - 1][score][prev_paint], val);
                             } else if (score > 0) {
-                                if (dp[apar - 1][score - 1][prev_paint] != -1) {
-                                    val = Math.min(dp[apar - 1][score - 1][prev_paint], val);
+                                if (memoization[apar - 1][score - 1][prev_paint] != -1) {
+                                    val = Math.min(memoization[apar - 1][score - 1][prev_paint], val);
                                 }
                             }
                             if (val != Long.MAX_VALUE) {
-                                dp[apar][score][paint] = val;
-                                if (!curr_apar_painted) {
-                                    dp[apar][score][paint] += cost[apar][paint];
+                                memoization[apar][score][paint] = val;
+                                if (!isCurrAptPainted) {
+                                    memoization[apar][score][paint] += buildingPaintCost[apar][paint];
                                 }
                             } else {
-                                dp[apar][score][paint] = -1;
+                                memoization[apar][score][paint] = -1;
                             }
                         }
                     }
@@ -96,7 +82,20 @@ public class HPDP {
             }
         }
 
-        return dp;
+        long ans = Long.MAX_VALUE;
+
+        for (int paint = 0; paint < noOfPaints; paint++) {
+
+            if (memoization[noOfBuildings - 1][splOfLoc - 1][paint] != -1) {
+                ans = Math.min(ans, memoization[noOfBuildings - 1][splOfLoc - 1][paint]);
+            }
+        }
+
+        if (ans == Long.MAX_VALUE)
+            return -1L;
+        else
+            return ans;
 
     }
+
 }
